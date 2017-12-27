5ecfdbb175b9c4589a140db012fa63bc8269c914
Vicky Mohammad <vimoh1213@gmail.com>
2017-12-27 05:46:50 -0500

    add code for country

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -13,13 +13,54 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     //dec object
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var stateLabel: UILabel!
+    
     //create array for the ui picker
     let states = ["Alaska", "Arkansas", "Alabama", "New York", "California", "Hawaii"]
+    
+    //create label for country
+    let countryLabel: UILabel = {
+        //dec var
+        let label = UILabel()
+        label.text = "Country"
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false//VERY IMPORTANT
+        //return label
+        return label
+    }()
+    //create constraint for the country label
+    func countryLabelConstraint(){
+        countryLabel.topAnchor.constraint(equalTo: stateLabel.bottomAnchor, constant: 40).isActive = true
+        countryLabel.leadingAnchor.constraint(equalTo: stateLabel.leadingAnchor).isActive = true
+        countryLabel.trailingAnchor.constraint(equalTo: stateLabel.trailingAnchor).isActive = true
+    }//end constarint
+    
+    //textfield for the country
+    let countryTextfield: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Enter the country..."
+        //field.layer.borderWidth = 1
+        //field.layer.borderColor = UIColor.gray.cgColor
+        field.translatesAutoresizingMaskIntoConstraints = false//VERY IMPORTANT
+        return field
+    }()
+    //create constraints for the country field
+    func countryTextFieldConstraint(){
+        countryTextfield.topAnchor.constraint(equalTo: countryLabel.bottomAnchor, constant: 5).isActive = true
+        countryTextfield.leadingAnchor.constraint(equalTo: countryLabel.leadingAnchor).isActive = true
+        countryTextfield.trailingAnchor.constraint(equalTo: countryLabel.trailingAnchor).isActive = true
+    }//end constraint
     
     override func viewDidLoad() {
         super.viewDidLoad()
         statePicker.dataSource = self
         statePicker.delegate = self
+        //add country label
+        view.addSubview(countryLabel)
+        countryLabelConstraint()
+        //add country field
+        view.addSubview(countryTextfield)
+        countryTextFieldConstraint()
     }//end func
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +70,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBAction func stateButtonPressed(_ sender: Any) {
         statePicker.isHidden = false
+        countryLabel.isHidden = true
     }//end listener
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -46,6 +88,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         stateButton.setTitle(states[row], for: UIControlState.normal)
         statePicker.isHidden = true
+        countryLabel.isHidden = false
     }//end uipicker
 
 }//end class
