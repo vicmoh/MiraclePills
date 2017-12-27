e7d20cbf7ea0863ce72362b521447e7d2db4d83c
Vicky Mohammad <vimoh1213@gmail.com>
2017-12-27 03:11:34 -0500

    init

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Vicky Mohammad on 2017-12-27.
+//  Copyright © 2017 Vicky Mohammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }//end func
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }//end func
+
+
+}//end class
+
