f19c8a3e3b1aefdd63552eaa0563022700e539ec
Vicky Mohammad <vimoh1213@gmail.com>
2017-12-27 04:03:49 -0500

    add picker

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    //dec object
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateButton: UIButton!
+    //create array for the ui picker
+    let states = ["Alaska", "Arkansas", "Alabama", "New York", "California", "Hawaii"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }//end func
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }//end func
 
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }//end listener
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }//end uipicker
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }//end uipicker
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }//end uipicker
 
 }//end class
 
